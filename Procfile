716b5c096504815ad7f43a241189f2f4fcbae300
skath <skat0010@student.monash.edu>
2021-11-20 22:04:31 +0530

    heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
-web: gunicorn REST_API_django.wsgi
+web: gunicorn solar.wsgi
+release: python manage.py makemigrations --noinput
+release: python manage.py collectstatic --noinput
+release: python manage.py migrate --noinput
--- solar/settings.py
+++ solar/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
